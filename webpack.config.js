7dfdefc85ce0eb918568da810e9dd946b76c43fa
condy <15979063786@163.com>
2016-05-21 17:25:50 +0800

    webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry:'./test.js',
+    output:{
+        path:__dirname+'/bundle/',
+        filename:'bundle.js'
+    },
+    watch:true,
+    module: {
+    loaders: [
+    {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+        query: {
+            presets: ['react', 'es2015']
+        }
+    }
+    ]
+    }
+}
