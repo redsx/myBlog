418bf9be3c70e3020f1bca6984e5a22c351a0ea1
condy <15979063786@163.com>
2016-06-07 22:47:57 +0800

    store

--- store/index.js
+++ store/index.js
@@ -3,7 +3,4 @@ import thunk from 'redux-thunk'
 import reducer from '../reducers'
 const finalCreactStore = applyMiddleware(thunk)(createStore);
 const store = finalCreactStore(reducer);
-store.subscribe(()=>{
-    console.log(store.getState());
-})
 export default store;
