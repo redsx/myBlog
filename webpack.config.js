fd64051c98f95e345d2a2f19e8539df546e3b01f
condy <15979063786@163.com>
2016-06-15 21:42:31 +0800

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
+var  webpack = require('webpack');
 module.exports = {
-    entry:'./test.jsx',
+    entry:{
+        bundle:'./test.jsx',
+        //vendor:['react','redux','react-dom','react-redux','reqwest','react-router','redux-thunk']
+    },
     output:{
         path:__dirname+'/bundle/',
         filename:'bundle.js'
     },
     watch:true,
-    module: {
-    loaders: [
-    {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
-        query: {
-            presets: ['react', 'es2015']
+    resolve:{
+        alias:{
+            //...
         }
-    }
-    ]
-    }
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        //new webpack.optimize.CommonsChunkPlugin('vendor',  'vendor.js')
+  ]
 }
